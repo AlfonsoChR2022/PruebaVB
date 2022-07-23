7181a56fa3dc946c26c7ed8ec4567c9f70ae0b89
Alfonso CH R <alfonso.chavez.r@gmail.com>
2022-07-23 18:37:18 -0500

    Agregue boton

--- PruebaVB/Form1.Designer.vb
+++ PruebaVB/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -35,11 +36,21 @@ Partial Class Form1
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Hola, soy Alfonso"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(284, 76)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -50,4 +61,5 @@ Partial Class Form1
 
     Friend WithEvents SaveFileDialog1 As SaveFileDialog
     Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
 End Class
